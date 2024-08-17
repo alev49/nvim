6f4bf974c53314e39124576d3fdc03df3337fb8c
alev49 <alev49@github.com>
2024-08-17 11:16:02 -0500

    Adding existing files to github

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,25 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim"
+    },
+    config = function()
+        require("neo-tree").setup({
+            event_handlers = {
+              {
+                event = "file_open_requested",
+                handler = function()
+                  -- auto close
+                  -- vimc.cmd("Neotree close")
+                  -- OR
+                    require("neo-tree.command").execute({ action = "close" })
+                end
+              },
+            vim.keymap.set('n', '<leader>o', ':Neotree filesystem reveal left <CR>')
+            }
+        })
+    end
+}
---
+++ lua/remaps.lua
@@ -0,0 +1,10 @@
+vim.g.mapleader = ' '
+vim.keymap.set('n', '<leader>ex', vim.cmd.Ex)
+vim.keymap.set("i", "<c-c>", "<esc>")
+vim.keymap.set({"i","n", "v"}, "<c-k>", "<esc>")
+vim.keymap.set("n", "<leader>q", ":q<cr>")
+vim.keymap.set("n", "<leader>h", ":h ")
+
+
+
+
