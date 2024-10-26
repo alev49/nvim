1d03985a270175df3746cd0ee3eceea4e762c03d
Alex Vidal <alex.jose.vidal@gmail.com>
2024-10-26 11:19:03 -0500

    added new save keymap

---
+++ lua/plugins/tender.lua
@@ -0,0 +1,9 @@
+return
+{
+    'jacoborus/tender.vim',
+    plugin = false,
+    priority = 1000,
+    confitg = function()
+        vim.cmd.colorscheme "tender"
+    end
+}
--- lua/remaps.lua
+++ lua/remaps.lua
@@ -4,6 +4,8 @@ vim.keymap.set("i", "<c-c>", "<esc>")
 vim.keymap.set({"i","n", "v"}, "<c-k>", "<esc>")
 vim.keymap.set("n", "<leader>q", ":q<cr>")
 vim.keymap.set("n", "<leader>h", ":h ")
+vim.keymap.set("n", "<leader>w", ":w<cr>")
+
 
 
 
